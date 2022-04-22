f4720515aff9ecb10b685539c2ce64cfb515f4f8
bchoUCSD <bcho@ucsd.edu>
2022-04-22 12:46:49 -0700

    own image link test
    Testing if the getLinks method will not read an image link.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -20,4 +20,13 @@ public class MarkdownParseTest {
         assertEquals("The link should be https://something.com","https://something.com",links.get(0));
         assertEquals("The links should be some-thing.html","some-thing.html",links.get(1));
     }
+
+    @Test
+    public void getImageLinkTest() throws IOException
+    {
+        Path fileName = Path.of("/Users/bryan/Documents/GitHub/markdown-parser/imageLink.md");
+        String fileContent = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(fileContent);
+        assertEquals("There should be nothing in the arrayList of links",0,links.size());
+    }
 }
